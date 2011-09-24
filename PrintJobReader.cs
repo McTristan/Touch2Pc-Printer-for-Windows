4f2cd7e6b2cfcfc974dddbd28baba512b662bc73
Sandro Lange <tristan@gmx.com>
2011-09-24 22:16:21 +0200

    Started working on issue #9 - Color- and Duplexmode is already recognized - just need to add some config settings
    Signed-off-by: Sandro Lange <tristan@gmx.com>

--- PrintJobReader.cs
+++ PrintJobReader.cs
@@ -9,6 +9,18 @@ using System.Diagnostics;
 
 namespace Touch2PcPrinter
 {
+    enum eColorMode
+    {
+        Color = 0,
+        BlackWhite = 1
+    }
+
+    enum eDuplexMode
+    {
+        NonDuplex = 0,
+        Duplex = 1
+    }
+
     internal class PrintJobReader
     {
         private const int JOB_LISTEN_PORT = 9100;
@@ -16,6 +28,11 @@ namespace Touch2PcPrinter
 
         private readonly TcpListener listener;
         private readonly string outputPath;
+
+        //sla 24.09.2011 - Color- & Duplex-Mode
+        public eColorMode ColorMode { get; set; }
+        public eDuplexMode DuplexMode { get; set; }
+        //..sla 24.09.2011 - Color- & Duplex-Mode
 
         public PrintJobReader(string outputPath)
         {
@@ -69,9 +86,45 @@ namespace Touch2PcPrinter
                         readCount += read;
                         stream.Write(buffer, 0, read);
                     } while (true);
-                    stream.Close();
-                    client.Close();
+
                     Trace.TraceInformation("Wrote print job file \"{0}\", {1} bytes in size", filePath, readCount);
+
+                    stream.Close();
+
+                    client.Close();
+                }
+
+                using (FileStream stream = File.OpenRead(filePath))
+                {
+                    stream.Seek(0x36, SeekOrigin.Begin); // get Duplexmode
+                    byte[] bArray = new byte[2];
+                    if (stream.Read(bArray, 0, 2) == 2)
+                    {
+                        if (bArray[0] == 0x4F && bArray[1] == 0x4E)
+                            DuplexMode = eDuplexMode.Duplex;
+                    }
+
+                    bArray = new byte[3];
+                    stream.Seek(0, SeekOrigin.Begin); // get BlackWhite
+                    for (int i = 0; i <= 512; i++)
+                    {
+                        if (stream.ReadByte() == 0x26)
+                        {
+                            if (stream.Read(bArray, 0, 3) == 3) //should contain "&b1M" for Black/White
+                            {
+                                if (bArray[0] == 0x62 && bArray[1] == 0x31 && bArray[2] == 0x4D)
+                                {
+                                    ColorMode = eColorMode.BlackWhite;                                    
+                                }
+                            }
+                            else
+                                stream.Seek(-2, SeekOrigin.Current);
+                        }
+                    }
+
+                    Trace.TraceInformation("Duplex set to: " + System.Enum.GetName(typeof(eDuplexMode), DuplexMode));
+                    Trace.TraceInformation("ColorMode set to: " + System.Enum.GetName(typeof(eColorMode), ColorMode));                    
+              
                 }
                 
             }
